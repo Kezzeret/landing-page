7d7af94d28c99d088da3a4c4ef1b67b3b72182fb
kezzeret <dmkotelevets@gmail.com>
2020-03-20 19:50:04 +0300

    4 blocks, need to check IE

--- JS/load-recent.js
+++ JS/load-recent.js
@@ -4,72 +4,74 @@ let recentArray = [
         title: 'Lorem ipsum dolor sit amet',
         text: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium.',
         dd: '30',
-        mm: 'Sep.'
+        mm: 'Sep.',
+        active: 0
     },
     {
         image: ["../img/recent/photo-2.png"],
         title: 'Lorem ipsum dolor sit amet',
         text: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium.',
         dd: '30',
-        mm: 'Sep.'
+        mm: 'Sep.',
+        active: 0
     },
     {
-        image: ["../img/recent/photo-3.png", "../img/recent/photo-4.jpg"],
+        image: ["../img/recent/photo-3.png"],
         title: 'Lorem ipsum dolor sit amet',
         text: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium.',
         dd: '30',
-        mm: 'Sep.'
+        mm: 'Sep.',
+        active: 0
     },
     {
-        image: ["../img/recent/photo-4.jpg"],
+        image: ["../img/recent/photo-4.jpg", "../img/recent/photo-3.png"],
         title: 'Lorem ipsum dolor sit amet',
         text: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium.',
         dd: '30',
-        mm: 'Sep.'
+        mm: 'Sep.',
+        active: 0
     },
 ];
 
-const firstItem = document.getElementById('first-recent-item');
-const secondItem = document.getElementById('second-recent-item');
-const thirdItem = document.getElementById('third-recent-item');
+const items = [
+    document.getElementById('first-recent-item'),
+    document.getElementById('second-recent-item'),
+    document.getElementById('third-recent-item')
+]
+
 const nextRecent = document.querySelector('.section-recent-inner-buttons-right');
 const previousRecent = document.querySelector('.section-recent-inner-buttons-left');
-const recentBubblesFirst = document.querySelector('.section-recent-inner-slider-item-image-bubbles.first');
 const recentBubbles = document.querySelectorAll('.section-recent-inner-slider-item-image-bubbles');
 
 function loadRecent() {
-    console.log(recentBubbles);
-    console.log(recentBubbles[0]);
-    console.log(recentBubbles[0].children);
-    firstItem.children[0].style.background = `url("${recentArray[0].image[0]}") no-repeat 50% 50%`;
-    firstItem.children[0].children[0].children[0].innerText = recentArray[0].dd;
-    firstItem.children[0].children[0].children[1].innerText = recentArray[0].mm;
-    firstItem.children[1].innerText = recentArray[0].title;
-    firstItem.children[2].innerText = recentArray[0].text;
-
-    secondItem.children[0].style.background = `url("${recentArray[1].image[0]}") no-repeat 50% 50%`;
-    secondItem.children[0].children[0].children[0].innerText = recentArray[1].dd;
-    secondItem.children[0].children[0].children[1].innerText = recentArray[1].mm;
-    secondItem.children[1].innerText = recentArray[1].title;
-    secondItem.children[2].innerText = recentArray[1].text;
-
-    thirdItem.children[0].style.background = `url("${recentArray[2].image[0]}") no-repeat 50% 50%`;
-    thirdItem.children[0].children[0].children[0].innerText = recentArray[2].dd;
-    thirdItem.children[0].children[0].children[1].innerText = recentArray[2].mm;
-    thirdItem.children[1].innerText = recentArray[2].title;
-    thirdItem.children[2].innerText = recentArray[2].text;
+    recentBubbles.forEach((item, i) => {
+        if (recentBubbles[i].children.length > 0)
+        {
+            let n = 0;
+            while (recentBubbles[i].children.length > 0) {
+                recentBubbles[i].children[n].remove();
+            }
+        }
+    });
+    items.forEach((item, i) => {
+        items[i].children[0].style.background = `url("${recentArray[i].image[recentArray[i].active]}") no-repeat 50% 50%`;
+        items[i].children[0].children[0].children[0].innerText = recentArray[i].dd;
+        items[i].children[0].children[0].children[1].innerText = recentArray[i].mm;
+        items[i].children[1].innerText = recentArray[i].title;
+        items[i].children[2].innerText = recentArray[i].text;
+    })
 
     loadRecentBubbles();
 }
 
-nextRecent.addEventListener('click', e => {
+previousRecent.addEventListener('click', e => {
     let item = recentArray[0];
     recentArray = recentArray.slice(1, recentArray.length);
     recentArray.push(item);
     loadRecent();
 });
 
-previousRecent.addEventListener('click', e => {
+nextRecent.addEventListener('click', e => {
     let item = recentArray[recentArray.length - 1];
     recentArray = recentArray.slice(0, recentArray.length - 1);
     recentArray.unshift(item);
@@ -86,9 +88,30 @@ function loadRecentBubbles() {
                 button.id = `recent-bubble-button-${n}-${i}`;
                 recentBubbles[n].append(button);
             })
-            document.getElementById(`recent-bubble-button-${n}-0`).className = 'section-slider-bubbles-button-empty';
+            document.getElementById(`recent-bubble-button-${n}-${recentArray[n].active}`).className = 'section-slider-bubbles-button-empty';
         }
         n++;
+    }
+}
+
+items[0].addEventListener('click', changeImage(items[0], 0));
+items[1].addEventListener('click', changeImage(items[1], 1));
+items[2].addEventListener('click', changeImage(items[2], 2));
+
+function changeImage(currentItem, currentIndex) {
+    return (e) => {
+        let number;
+        let dots = Array.from(currentItem.children[0].children[1].children);
+        const targetBubble = e.target.closest('button');
+        if (!targetBubble)
+            return;
+        number = dots.findIndex(dot => dot === targetBubble);
+        recentArray[currentIndex].active = number;
+        dots.forEach((item, i) => {
+            dots[i].className = 'section-slider-bubbles-button-full';
+        })
+        dots[number].className = 'section-slider-bubbles-button-empty';
+        currentItem.children[0].style.background = `url("${recentArray[currentIndex].image[number]}") no-repeat 50% 50%`;
     }
 }
 
