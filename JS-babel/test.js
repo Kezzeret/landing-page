d09097deb1c67e103660e5636702f58a08d49ff5
kezzeret <dmkotelevets@gmail.com>
2020-03-22 20:55:02 +0300

    block and footer added. need to check mixins

--- JS-babel/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var str = 'ES6';
-console.log("Hello ".concat(str));
-var a = 3;
--- JS/mobile-menu_IE.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var burgerMenu = document.getElementById('burger-menu');
-var mobileMenu = document.getElementById('mobile-menu');
-burgerMenu.addEventListener('click', function (e) {
-    console.log('click');
-    mobileMenu.classList.toggle('active');
-    burgerMenu.classList.toggle('active');
-});
